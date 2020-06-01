666daed21b1d6454a95bceaa0a43f790c7e816e2
Maciej Choromański <maciej.choromanski@wp.pl>
2020-06-01 23:09:24 +0200

    Add example of 'Builder' design pattern (#3)
    * Add example of Builder design pattern
    
    * Update README

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Examples of some design patterns in Python
 
 1. [Abstract Factory](design_patterns_python/abstract_factory.py)
 2. [Adapter](design_patterns_python/adapter.py)
+3. [Builder](design_patterns_python/builder.py)
---
+++ design_patterns_python/builder.py
@@ -0,0 +1,76 @@
+# Example of 'Builder' design pattern
+
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+
+class Player:
+    """Product"""
+
+    _name = ''
+
+    def set_name(self, name: str) -> None:
+        self._name = name
+
+    def show(self) -> None:
+        print(self._name)
+
+
+class PlayerBuilder(ABC):
+    """Builder"""
+
+    @property
+    @abstractmethod
+    def player(self) -> Player:
+        pass
+
+    @abstractmethod
+    def build_player(self) -> None:
+        pass
+
+
+class FlashPlayerCreator(PlayerBuilder):
+    """Concrete builder A"""
+
+    _player = Player()
+
+    @property
+    def player(self) -> Player:
+        return self._player
+
+    def build_player(self) -> None:
+        self._player.set_name('Flash Player')
+
+
+class HTMLPlayerCreator(PlayerBuilder):
+    """Concrete builder B"""
+
+    _player = Player()
+
+    @property
+    def player(self) -> Player:
+        return self._player
+
+    def build_player(self) -> None:
+        self._player.set_name('HTML Player')
+
+
+class Generator:
+    """Director"""
+
+    @staticmethod
+    def build(player_builder: PlayerBuilder) -> None:
+        player_builder.build_player()
+
+
+if __name__ == '__main__':
+    html_creator = HTMLPlayerCreator()
+    flash_creator = FlashPlayerCreator()
+    generator = Generator()
+
+    generator.build(html_creator)
+    html_creator.player.show()
+
+    generator.build(flash_creator)
+    flash_creator.player.show()
