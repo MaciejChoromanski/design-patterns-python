f60cd6b5f21307a64a2c8a4fe3cf1a5c5715fd0b
Maciej Choromański <maciej.choromanski@wp.pl>
2020-06-01 22:05:04 +0200

    Add example of 'Adapter' design pattern (#2)
    * Add example of 'Adapter' design pattern
    
    * Update README

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Examples of some design patterns in Python
 ## Previewed patterns
 
 1. [Abstract Factory](design_patterns_python/abstract_factory.py)
+2. [Adapter](design_patterns_python/adapter.py)
---
+++ design_patterns_python/adapter.py
@@ -0,0 +1,35 @@
+# Example of Adapter design pattern
+
+from abc import ABC, abstractmethod
+
+
+class Sensor(ABC):
+    """Target"""
+
+    @abstractmethod
+    def get_temperature(self) -> float:
+        pass
+
+
+class FahrenheitSensor:
+    """Adaptee"""
+
+    temperature = 99.4
+
+    def get_temperature(self) -> float:
+        return self.temperature
+
+
+class Adapter(Sensor):
+    """Adapter"""
+
+    def __init__(self, fahrenheit_sensor: FahrenheitSensor):
+        self.fahrenheit_sensor = fahrenheit_sensor
+
+    def get_temperature(self) -> float:
+        return (self.fahrenheit_sensor.get_temperature() - 32) * 5 / 9
+
+
+if __name__ == '__main__':
+    celsius = Adapter(FahrenheitSensor())
+    print(f'99.4 °F = {celsius.get_temperature()} °C')
