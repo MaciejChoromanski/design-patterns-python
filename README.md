7c96086a64bae30f9a4e03f2bed4d03b39e7ab03
Maciej Choromański <maciej.choromanski@wp.pl>
2020-06-02 20:26:58 +0200

    Add example of 'Decorator' design pattern (#10)
    * Add example of 'Decorator' design pattern
    
    * Update README

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Examples of some design patterns in Python
 7. [Observer](design_patterns_python/observer.py)
 8. [Strategy](design_patterns_python/strategy.py)
 9. [Prototype](design_patterns_python/prototype.py)
+10. [Decorator](design_patterns_python/decorator.py)
---
+++ design_patterns_python/decorator.py
@@ -0,0 +1,110 @@
+# Example of 'Decorator' design pattern
+
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class ObjectFromLibrary(ABC):
+    """Component"""
+
+    _amount: int = None
+
+    @property
+    def amount(self) -> int:
+        return self._amount
+
+    @amount.setter
+    def amount(self, amount: int) -> None:
+        self._amount = amount
+
+    @abstractmethod
+    def show(self) -> None:
+        pass
+
+
+class Book(ObjectFromLibrary):
+    """Concrete Component A"""
+
+    def __init__(self, author: str, title: str, amount: int) -> None:
+        self._author = author
+        self._title = title
+        self.amount = amount
+
+    def show(self) -> None:
+        print(
+            f'---Book---\n'
+            f'Author: {self._author}\n'
+            f'Title: {self._title}\n'
+            f'Amount: {self.amount}\n'
+        )
+
+
+class Movie(ObjectFromLibrary):
+    """Concrete Component B"""
+
+    def __init__(
+            self, director: str, title: str, amount: int, length: float
+    ) -> None:
+        self._director = director
+        self._title = title
+        self.amount = amount
+        self._length = length
+
+    def show(self) -> None:
+        print(
+            f'---Movie---\n'
+            f'Author: {self._director}\n'
+            f'Title: {self._title}\n'
+            f'Amount: {self.amount}\n'
+            f'Length: {self._length}\n'
+        )
+
+
+class Decorator(ObjectFromLibrary):
+    """Decorator"""
+
+    _component: ObjectFromLibrary = None
+
+    def __init__(self, component: ObjectFromLibrary) -> None:
+        self._component = component
+
+    @property
+    def component(self) -> ObjectFromLibrary:
+        return self._component
+
+    def show(self) -> None:
+        self._component.show()
+
+
+class Rentable(Decorator):
+    """Concrete Decorator A"""
+
+    _renters: List[str] = []
+
+    def rent_object(self, renter) -> None:
+        self._renters.append(renter)
+        self.component.amount -= 1
+
+    def return_object(self, renter) -> None:
+        self._renters.remove(renter)
+        self.component.amount += 1
+
+    def show(self) -> None:
+        super().show()
+        for renter in self._renters:
+            print(f'Renter: {renter}')
+
+
+if __name__ == '__main__':
+    book = Book('Eric Matthes', 'Python Crash Course', 2)
+    book.show()
+    movie = Movie('Joe Johnston', 'Jumanji', 3, 2.05)
+    movie.show()
+
+    rentable_movie = Rentable(movie)
+    rentable_movie.rent_object('Renter no. 1')
+    rentable_movie.rent_object('Renter no. 2')
+    rentable_movie.return_object('Renter no. 1')
+    rentable_movie.show()
